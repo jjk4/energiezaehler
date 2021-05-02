89bb3f9a0c8f4b4fc880d7e6aa026cedc4c8b572
jjk1 <37013673+jjk4@users.noreply.github.com>
2021-05-02 12:45:41 +0200

    Add files via upload

---
+++ rawdata.php
@@ -0,0 +1,32 @@
+		<?php 
+			$site_name = "Auswertung - Rohdaten";
+			include ("header.php"); 
+		?>
+		<form action="submitquery.php">
+			<label>Zähler:
+				<select name="zaehler">
+					<?php
+						foreach ($config['zaehler'] as $key => $value) {
+   							echo "<option value=" . $key . ">" . $value . "</option>", PHP_EOL;
+						};
+					?>
+				</select>
+			</label><br><br>
+			<label>Von: 
+				<?php 
+					$timestamp = time(); 
+					$datum = date("Y-m-d", $timestamp) . "T" . date("H:i", $timestamp);
+					echo("<input name=\"startdatum\" type=\"datetime-local\" value=$datum>\n"); 
+				?>
+			</label>
+			<label>Bis: 
+				<?php 
+					$timestamp = time(); 
+					$datum = date("Y-m-d", $timestamp) . "T" . date("H:i", $timestamp);
+					echo("<input name=\"enddatum\" type=\"datetime-local\" value=$datum>\n"); 
+				?>
+			</label><br><br>
+			<input type="submit">
+		</form>
+	</body>
+</html>
