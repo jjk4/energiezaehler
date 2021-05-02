c578387ae21136d0bad0835aa9f5333355e39213
jjk1 <37013673+jjk4@users.noreply.github.com>
2021-05-02 12:42:11 +0200

    Add files via upload

---
+++ app/rawdata.php
@@ -0,0 +1,36 @@
+<html>
+	<head>
+		<title>Auswertung - Energiezählerapp</title>
+		<link rel="stylesheet" href="style.css">
+
+	</head>
+	<body>
+		<h1>Rohdaten</h1>
+		<form action="submitquery.php">
+			<label>Zähler:
+				<select name="zaehler">
+					<?php
+						foreach ($config['zaehler'] as $key => $value) {
+   							echo "<option value=" . $key . ">" . $value . "</option>", PHP_EOL;
+						};
+					?>
+				</select>
+			</label><br><br>
+			<label>Von: 
+				<?php 
+					$timestamp = time(); 
+					$datum = date("Y-m-d", $timestamp) . "T" . date("H:i", $timestamp);
+					echo("<input name=\"startdatum\" type=\"datetime-local\" value=$datum>\n"); 
+				?>
+			</label><br><br>
+			<label>Bis: 
+				<?php 
+					$timestamp = time(); 
+					$datum = date("Y-m-d", $timestamp) . "T" . date("H:i", $timestamp);
+					echo("<input name=\"enddatum\" type=\"datetime-local\" value=$datum>\n"); 
+				?>
+			</label><br><br>
+			<input type="submit">
+		</form>
+	</body>
+</html>
