179b875deb850a982f6781db0ebd958bfcb47f50
jjk1 <37013673+jjk4@users.noreply.github.com>
2021-05-02 12:43:53 +0200

    Update analysis.php

--- analysis.php
+++ analysis.php
@@ -2,6 +2,15 @@
 			$site_name = "Auswertung";
 			include ("header.php"); 
 		?>
-		<iframe id="grafana" src="http://solaranzeige.fritz.box:3000/d/zFMXA8Rgk/energiezahler?orgId=1&from=now-30d&to=now&kiosk=tv">
+		<a class="analysislink" href="grafana.php">
+			<div>
+				Zur Grafana visualisierung
+			</div>
+		</a><br>
+		<a class="analysislink" href="rawdata.php">
+			<div>
+				Zu den Rohdaten
+			</div>
+		</a>
 	</body>
 </html>
