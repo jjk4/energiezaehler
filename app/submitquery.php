84208cc59b708b9bde9275644798a583b1938893
jjk1 <37013673+jjk4@users.noreply.github.com>
2021-05-02 12:42:40 +0200

    Add files via upload

---
+++ app/submitquery.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Rohdaten - Energiezählerapp</title>
+		<link rel="stylesheet" href="style.css">
+
+	</head>
+	<body>
+		<h1>Rohdaten</h1>
+		<a href=rawdata.php>Zurück</a><br>
+		<?php
+			$zaehler = $_GET["zaehler"];
+			$startdatum = $_GET["startdatum"] . ":00Z";
+			$enddatum = $_GET["enddatum"] . ":00Z";
+			echo shell_exec("python3 ../query.py $startdatum $enddatum $zaehler");
+		?>
+	</body>
+</html>
