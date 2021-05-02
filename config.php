87a1c3fa0a933d92c90780b2819908df6db47ba9
jjk1 <37013673+jjk4@users.noreply.github.com>
2021-05-02 12:44:11 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+	return [
+		'zaehler' => [
+			'pv_garage' => 'PV Garage',
+			'wasser' => 'Wasser',
+			'strom' => 'Strom',
+			'garten' => 'Garten',
+			'lueftung_heizraum' => 'Lüftung Heizraum',
+			'display_flur' => 'Display Flur',
+			'pv_haus' => 'PV Haus',
+		],
+		
+	];
+?>
