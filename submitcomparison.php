0f63827ca16733ca3de04277e9d0439711681585
jjk1 <37013673+jjk4@users.noreply.github.com>
2021-05-13 10:17:54 +0200

    Delete submitcomparison.php

--- submitcomparison.php
+++
@@ -1,8 +0,0 @@
-		<?php 
-			$site_name = "Vergleich - Auswertung";
-			include ("header.php"); 
-		?>
-		Die Vergleichsseite ist derzeit noch in Entwicklung<br>
-		<a href="analysis.php">Zurück zur Auswertungsseite</a>
-	</body>
-</html>
