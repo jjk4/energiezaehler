8e5d0be5179ed05d6ab75a118f00e4f3420aafa7
jjk1 <37013673+jjk4@users.noreply.github.com>
2021-10-23 17:56:13 +0200

    Delete installation.php

--- installation.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$config = json_decode(file_get_contents('config.json'), true);
-	$new_configuration['host'] = $argv[1];
-	$new_configuration['port'] = $argv[2];
-	$new_configuration['database'] = $argv[3];
-	$new_configuration['installationpath'] = $argv[4];
-	$new_configuration['zaehler'] = $config['zaehler'];
-	file_put_contents("config.json",json_encode($new_configuration));
-?>
