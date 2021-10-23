42131b5fc5684e1e326a400cc9b811e7681b6124
jjk1 <37013673+jjk4@users.noreply.github.com>
2021-10-23 17:56:52 +0200

    Delete grafana.php

--- grafana.php
+++
@@ -1,9 +0,0 @@
-		<?php 
-			$site_name = "Grafana - Auswertung";
-			include ("header.php"); 
-		?>
-		<div class="content">
-			<iframe id="grafana" src="http://solaranzeige.fritz.box:3000/d/zFMXA8Rgk/energiezahler?orgId=1&from=now-30d&to=now&kiosk=tv">
-		</div>
-	</body>
-</html>
