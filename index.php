c6ec4c49cae0da0a8a6858b82656181cfdb2b0ac
jjk4 <shaun7090@gmail.com>
2022-04-04 17:58:16 +0200

    Vorbereitung für v2

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	$site_name = "Startseite";
-	include ("header.php"); 
-?>
-Version: 2.0
-<?php include("footer.php");?>
--- test.php
+++
@@ -1,7 +0,0 @@
-<?php include("header.php");?>
-<script>
-    $.get( "visu-backend.php", { v: "month", m: "16", d: "2019-08"} )
-        .done(function( data ) {
-        alert( "Data Loaded: " + data );
-    });
-</script>
